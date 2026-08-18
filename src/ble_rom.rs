e059947b7c25dc0e8fe98823321ac09f04595752
Roman Popov <atfpom4h@gmail.com>
2026-08-18 09:36:16 +0300

    Route LE controller capability commands

--- src/ble_rom.rs
+++ src/ble_rom.rs
@@ -1,4 +1,4 @@
-use crate::{arm_abi, bm_rom, cbtimer_rom, hci_extra, hci_rom, hci_security, ll_crypto, ll_rom, osal_power};
+use crate::{arm_abi, bm_rom, cbtimer_rom, hci_caps, hci_extra, hci_rom, hci_security, ll_crypto, ll_rom, osal_power};
 use zmu_cortex_m::bus::Bus;
 use zmu_cortex_m::core::register::{BaseReg, Reg};
 use zmu_cortex_m::Processor;
@@ -21,6 +21,9 @@ pub fn handle(cpu: &mut Processor, rng: &mut u32) -> bool {
         return true;
     }
     if hci_security::handle(cpu) {
+        return true;
+    }
+    if hci_caps::handle(cpu) {
         return true;
     }
     if hci_extra::handle(cpu, rng) {
